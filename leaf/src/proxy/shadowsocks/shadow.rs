cfc25680a8baefaac99d6edd90d307aed2eef46a
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-09-02 10:13:55 +0800

    fix

--- leaf/src/proxy/shadowsocks/shadow.rs
+++ leaf/src/proxy/shadowsocks/shadow.rs
@@ -191,7 +191,7 @@ where
                             }
                         }
 
-                        me.read_buf.clear();
+                        //me.read_buf.clear();
                     }
                     // ready to read plaintext payload into buf
                     me.read_state = ReadState::PendingData(n);
